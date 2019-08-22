4650daf357260dab95c62f619393e793eb75e64e
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-08-22 14:03:51 +0200

    Dev mode via NODE_ENV

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import 'src/util/polyfill.js'
 import myModule from 'src/myModule.js'
 
-// Do dev-testing to your module here, just remeber to remove/comment it when builing for production
-// window.myModule = myModule
+// Do dev-testing to your module here
+if (process.env.NODE_ENV === 'development') {
+  console.log('🚀 Dev mode enabled')
+  window.myModule = myModule
+}
 
 // Library export
 export default myModule
